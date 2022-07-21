6e0f2a39236e5516d5ece4784e6d9a3f013b8f23
Pratham <upratham07@gmail.com>
2022-07-21 13:43:29 +0530

    Initial commit

---
+++ src/main/java/com/Employee/management/EmployeeData.java
@@ -0,0 +1,24 @@
+package com.Employee.management;
+
+
+import java.sql.*;
+import javax.swing.*;
+public class EmployeeData {
+
+    public static Connection ConnectDB()
+    {
+        try {
+            Class.forName("org.sqlite.jdbc");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:empdata.db");
+            JOptionPane.showMessageDialog(null,"Connection Made");
+            return conn;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,"Connection Made");
+            return null;
+        }
+    }
+
+}
+
